6be96ab121e679427d925d069bc6f3daf3493198
mkov77 <kovalsky.michaela@gmail.com>
2024-10-20 12:47:06 -0600

    menu position fix

--- src/pages/adminPage.tsx
+++ src/pages/adminPage.tsx
@@ -245,7 +245,7 @@ function AdminPage() {
                     withinPortal
                   >
                     <Menu.Target>
-                      <ActionIcon variant="subtle" color="gray">
+                      <ActionIcon variant="subtle" color="white" style={{ marginRight: '15px' }}>
                         <IconDots style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
                       </ActionIcon>
                     </Menu.Target>
